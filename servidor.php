ec595d6f021bd460daf2b90099fe1bbcf4102070
enriquefnt <enriquefinetti@gmail.com>
2022-11-07 16:05:20 -0300

    nada

--- servidor.php
+++ servidor.php
@@ -1,4 +1,20 @@
 <?php
+include __DIR__ . '/conect.php';
+
+
+$stmt = $pdo->prepare("SELECT CONCAT(Nombres,' ',Apellidos) as nomApe, id_datos_benef, DNI FROM datos_benef" );
+
+
+
+
+
+
+
+
+
+
+
+
 // DUMMY SEARCH SCRIPT - DO YOUR OWN!
 // E.G. Search database - SELECT * FROM `table` WHERE `name` LIKE "%SEARCH%"
 
